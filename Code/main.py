029f22d0d7af877533919bc226357f1b035a8c7f
Hassan Mahmood Khan <97694796+HassanMahmoodKhan@users.noreply.github.com>
2024-06-09 17:09:56 -0400

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -16,20 +16,20 @@ def main():
     X,y = preprocessing(data) 
     
     # Uncomment to perform hyperparamter grid search
-    # grid_search = gs_pipeline(X, y) 
-    # print(grid_search)
+    grid_search = gs_pipeline(X, y) 
+    print(grid_search)
     
     # Split the dataset into training and test datasets
     X_train, X_test, y_train, y_test = my_train_test_split(X,y)
 
     # Uncomment to perform model training
-    # train(X_train, y_train)
+    train(X_train, y_train)
 
     # ONNX Conversion
     onnx_conversion(X, y)
 
     # Model Evaluation/Inference
-    # evaluate(X_train, y_train, X_test, y_test)
+    evaluate(X_train, y_train, X_test, y_test)
 
     # ONNX Runtime Inference
     onnx_inference(X_test, y_test)
